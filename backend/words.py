385b6eef8955c0ea23c54bf5b4ae16f4c4c19aea
Josh Larson <joshlarson2015@gmail.com>
2022-02-13 16:55:09 -0600

    Moved word lists into the resources folder

--- backend/words.py
+++ backend/words.py
@@ -1,10 +1,10 @@
 import random
 
 
-with open("words_likely.txt", "r") as file:
+with open("resources/words_likely.txt", "r") as file:
 	words_likely = [word.strip() for word in file.readlines() if len(word.strip()) == 5]
 
-with open("words_all.txt", "r") as file:
+with open("resources/words_all.txt", "r") as file:
 	words = [word.strip() for word in file.readlines() if len(word.strip()) == 5]
 
 random.shuffle(words_likely)
